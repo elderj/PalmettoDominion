cfa252571f25800b3e29eff86704ac61ad2717c5
Joseph Elder <jelde010@odu.edu>
2018-10-16 21:23:54 -0400

    Added markdown and route switching functionality to the Recipe list.

---
+++ src/components/BasicExample.js
@@ -0,0 +1,39 @@
+/* eslint-disable import/no-webpack-loader-syntax */
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import HelloWorld from '!babel-loader!mdx-loader!../data/recipes/simpleSerranoHotSauce.mdx';
+
+export const BasicExample = () => (
+    <Router>
+        <div>
+            <Route path="/Recipes" component={RecipeList} />
+        </div>
+    </Router>
+);
+
+export const RecipeList = ({ match }) => (
+    <div>
+
+        <ul>
+            <li>
+                <Link to={`${match.url}/simpleSerranoHotSauce`}>Simple Serrano Hot Sauce</Link>
+            </li>
+            <li>
+                <Link to={`${match.url}/components`}>Components</Link>
+            </li>
+        </ul>
+
+        <Route path={`${match.path}/:topicId`} component={Topic} />
+        <Route
+            exact
+            path={match.path}
+            render={() => <h3>Please select a topic.</h3>}
+        />
+    </div>
+);
+
+const Topic = ({ match }) => (
+    <div>
+        <h3>{match.params.topicId === 'simpleSerranoHotSauce' ? (<HelloWorld/>) : (<p>Not it </p>)}</h3>
+    </div>
+);
--- src/components/RecipesPage.js
+++ src/components/RecipesPage.js
@@ -1,14 +1,42 @@
 import React, { Fragment } from 'react';
+import { BasicExample, } from './BasicExample' 
+
+
 
 const RecipesPage = () => (
   <Fragment>
+  <br />
+  <div className="Aligner">
+    <div className="Aligner-item">
+      <h1 className="font-actionj">Recipes</h1>
+    </div>
     <br />
-    <div className="Aligner">
-      <div className="Aligner-item">
-        <h1 className="font-actionj">Recipes</h1>
+  </div>
+
+  <div className="Aligner">
+    <div
+      className="Aligner-item"
+      style={{
+        backgroundColor: '#F0F0F0',
+        width: '400vw',
+        borderRadius: '25px',
+        padding: '8px',
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: '#fff',
+
+          borderRadius: '20px',
+          padding: '8px',
+        }}
+      >
+        <BasicExample />
       </div>
     </div>
-  </Fragment>
+  </div>
+  <br />
+</Fragment>
 );
 
 export default RecipesPage;
