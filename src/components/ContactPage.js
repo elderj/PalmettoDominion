96c53e939365f3ab492e28b3c85a6d081703c009
Joseph Elder <jelde010@odu.edu>
2018-11-23 18:56:02 -0500

    Added stub form for contact

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,5 +1,5 @@
-import React, { Fragment } from 'react';
-
+import React, { Fragment } from "react";
+import { Button, Form, Input } from "antd";
 class ContactPage extends React.Component {
   render() {
     return (
@@ -8,9 +8,55 @@ class ContactPage extends React.Component {
         <div className="Aligner">
           <div className="Aligner-item">
             <h1 className="font-actionj">What's Up?</h1>
-            <p>Contact Page</p>
+          </div>
+          <br />
+        </div>
+
+        <div className="Aligner">
+          <div
+            className="Aligner-item"
+            style={{
+              backgroundColor: "#F0F0F0",
+              width: "400vw",
+              borderRadius: "25px",
+              padding: "8px"
+            }}
+          >
+            <div
+              className="Aligner"
+              style={{
+                backgroundColor: "#fff",
+
+                borderRadius: "20px",
+                padding: "8px"
+              }}
+            >
+              <Form>
+                <br />
+
+                <p style={{ padding: "2vw" }}>
+                  Have some questions regarding hot sauce or peppers? Got a
+                  suggestion of how this site could be better? Want to become a
+                  contributor? Shoot us an email and let us know!
+                </p>
+                <Form.Item>
+                  <Input addonBefore="Your Name" placeholder="Name" />
+                </Form.Item>
+                <Form.Item>
+                  <Input addonBefore="Your Email" placeholder="Your Email" />
+                </Form.Item>
+                <Form.Item>
+                  <Input addonBefore="  Subject " placeholder="Subject" />
+                </Form.Item>
+                <Form.Item>
+                  <Input.TextArea addonBefore="Message" placeholder="Message" />
+                </Form.Item>
+                <Button>Submit</Button>
+              </Form>
+            </div>
           </div>
         </div>
+        <br />
       </Fragment>
     );
   }
